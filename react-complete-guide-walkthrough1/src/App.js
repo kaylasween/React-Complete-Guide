6b5b44191bc5ff0e8989e3e100dd6ed2a7087e2c
Kayla Sween <kayla@kaylasween.com>
2019-11-07 13:09:23 -0600

    added media query to person component.

--- react-complete-guide-walkthrough1/src/App.js
+++ react-complete-guide-walkthrough1/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import Radium from 'radium'
+import Radium, { StyleRoot } from 'radium'
 import './App.css'
 
 import Person from './Person/Person'
@@ -100,18 +100,20 @@ class App extends Component {
 
     return (
       // lowercase elements reserved for JSX native elements.
-      <div className="App">
-        <h1>Hi, I'm a React App!</h1>
-        <p className={classes.join(' ')}>This is really working!</p>
+      <StyleRoot>
+        <div className="App">
+          <h1>Hi, I'm a React App!</h1>
+          <p className={classes.join(' ')}>This is really working!</p>
 
-        <button 
-          onClick={() => this.togglePersonsHandler()}
-          style={style}
-        >
-          Toggle Persons
-        </button>
-        {persons}
-      </div>
+          <button 
+            onClick={() => this.togglePersonsHandler()}
+            style={style}
+          >
+            Toggle Persons
+          </button>
+          {persons}
+        </div>
+      </StyleRoot>
     )
     // return React.createElement('div, {className: 'App'}, React.createElement('h1', null, 'Hi, I'm a React App!'));
   }
--- react-complete-guide-walkthrough1/src/Person/Person.js
+++ react-complete-guide-walkthrough1/src/Person/Person.js
@@ -5,8 +5,14 @@ import './Person.css'
 // stateless/presentational/dumb component. have more of these than stateful components. 
 
 const person = (props) => {
+    const style = {
+        '@media (min-width: 500px)': {
+            width: '100vw'
+        }
+    }
+
     return (
-        <div className="Person">
+        <div className="Person" style={style}>
             {/* can pass method as props */}
             <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
             <p>{props.children}</p>
