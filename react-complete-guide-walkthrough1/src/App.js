fb6d01734ea8133c7a97b9f8aa2011290ee6eb2f
Kayla Sween <kayla@kaylasween.com>
2019-11-06 18:53:37 -0600

    readding the change name handler.

--- react-complete-guide-walkthrough1/src/App.js
+++ react-complete-guide-walkthrough1/src/App.js
@@ -9,37 +9,46 @@ import Person from './Person/Person'
 class App extends Component {
   state = {
     persons: [
-      { name: 'Max', age: 28 },
-      { name: 'Manu', age: 29 },
-      { name: 'Stephanie', age: 26 }
+      { id: 1, name: 'Max', age: 28 },
+      { id: 2, name: 'Manu', age: 29 },
+      { id: 3, name: 'Stephanie', age: 26 }
     ],
     someOtherState: 'some other value',
     showPersons: false
   }
 
-  switchNameHandler = (newName) => {
-    this.setState({
-      persons: [
-        { name: newName, age: 28 },
-        { name: 'Manu', age: 29 },
-        { name: 'Stephanie', age: 27 }
-      ]
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex((p) => {
+      return p.id === id
     })
-  }
 
-  nameChangedHandler = (event) => {
-    this.setState({
-      persons: [
-        { name: 'Max', age: 28 },
-        { name: event.target.value, age: 29 },
-        { name: 'Stephanie', age: 26 }
-      ]
-    })
+    const person = {
+      ...this.state.persons[personIndex]
+    }
+
+    //const person = Object.assign({}, this.state.persons[personIndex]) can also be done
+
+    person.name = event.target.value
+
+    const persons = [...this.state.persons]
+
+    persons[personIndex] = person
+
+    this.setState({ persons: persons })
   }
 
   togglePersonsHandler = () => {
     const doesShow = this.state.showPersons
     this.setState({ showPersons: !doesShow })
+  }
+
+  deletePersonHandler = (personIndex) => {
+    //so we can copy without issues and without changing the original object, we add slice. Always update state in an immutable fashion.
+    // const persons = this.state.persons.slice()
+    //also can use the spread operator
+    const persons = [...this.state.persons]
+    persons.splice(personIndex, 1)
+    this.setState({ persons: persons })
   }
 
   render() {
@@ -55,8 +64,13 @@ class App extends Component {
     if (this.state.showPersons) {
       persons = (
         <div>
-          {this.state.persons.map(person => { 
-            return <Person name={person.name} age={person.age} />
+          {this.state.persons.map((person, index) => { 
+            return <Person 
+                      key={person.id} 
+                      name={person.name} 
+                      age={person.age} 
+                      click={() => this.deletePersonHandler(index)}
+                      changed={(event) => this.nameChangedHandler(event, person.id)} />
           })}
         </div> 
       )
@@ -67,7 +81,7 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I'm a React App!</h1>
         <p>This is really working!</p>
-        {/* two ways to call switchNameHandler here */}
+
         <button 
           onClick={() => this.togglePersonsHandler()}
           style={style}
