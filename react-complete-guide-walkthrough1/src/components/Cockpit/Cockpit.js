f191b019e34eb92572d1f81353151be91a7bf9ce
Kayla Sween <kayla@kaylasween.com>
2019-11-08 18:39:44 -0600

    learning about useEffect hook.

--- react-complete-guide-walkthrough1/src/components/Cockpit/Cockpit.js
+++ react-complete-guide-walkthrough1/src/components/Cockpit/Cockpit.js
@@ -1,7 +1,17 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import styles from './Cockpit.module.css'
 
 const cockpit = (props) => {
+    //useState can be used instead of getDerivedState
+    useEffect(() => {
+        console.log('[Cockpit.js] useEffect')
+        //http request...
+        //componentDidMount and componentDidUpdate in one method
+        setTimeout(() => {
+            alert('something')
+        }, 1000)
+    }, []) //props.persons if we want to rerun it when persons changes
+
     const classes = []
     let btnClass = '';
 
