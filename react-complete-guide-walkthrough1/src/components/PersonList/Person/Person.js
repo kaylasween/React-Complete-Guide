c8f238f58deb8d3280fbad5dbd6f61e2aeac82c6
Kayla Sween <kayla@kaylasween.com>
2019-11-10 15:23:15 -0600

    learning how to use ref.

--- react-complete-guide-walkthrough1/src/components/PersonList/Person/Person.js
+++ react-complete-guide-walkthrough1/src/components/PersonList/Person/Person.js
@@ -12,6 +12,17 @@ class Person extends Component {
     // if (rnd > 0.7) {
     //     throw new Error('Something went wrong')
     // }
+
+    constructor(props) {
+        super(props)
+        this.inputElementRef = React.createRef()
+    }
+
+    componentDidMount() {
+        // this.inputElement.focus()
+        this.inputElementRef.current.focus()
+    }
+
     render() {
         console.log('[Person.js] rendering...')
         return (
@@ -21,7 +32,12 @@ class Person extends Component {
                 {/* can pass method as props */}
                 <p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>
                 <p>{this.props.children}</p>
-                <input type="text" onChange={this.props.changed} value={this.props.name} />
+                <input 
+                    // ref={(inputEl) => {this.inputElement = inputEl}} older version
+                    ref={this.inputElementRef}
+                    type="text" 
+                    onChange={this.props.changed} 
+                    value={this.props.name} />
             </> 
             //</React.Fragment>
             // </div>
