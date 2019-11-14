0d7ac76eb5e5b421f0f49f0e66a9fe94eb7161a0
Kayla Sween <kayla@kaylasween.com>
2019-11-14 17:07:29 -0600

    flattening array so that we can easily check if it's empty. adding a message to ask the user to start adding ingredients if there are currently none.

--- burger-builder/src/components/Burger/Burger.js
+++ burger-builder/src/components/Burger/Burger.js
@@ -3,11 +3,17 @@ import styles from './Burger.module.css'
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
 
 const burger = (props) => {
-  const transformedIngredients = Object.keys(props.ingredients).map(ingredientKey => {
+  let transformedIngredients = Object.keys(props.ingredients).map(ingredientKey => {
     return [...Array(props.ingredients[ingredientKey])].map((_, index) => {
       return <BurgerIngredient key={ingredientKey + index} type={ingredientKey} />
     })
-  })
+  }).reduce((arr, el) => {
+    return arr.concat(el)
+  }, [])
+
+  if(!transformedIngredients.length) {
+    transformedIngredients = <p>Please start adding ingredients!</p>
+  }
 
   return (
     <div className={styles.burger}>
