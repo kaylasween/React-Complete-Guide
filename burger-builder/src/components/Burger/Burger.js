5e545b50e8c10ba44b576ab782d93b2fc8a04035
Kayla Sween <kayla@kaylasween.com>
2019-11-14 17:01:38 -0600

    starting to make the burger builder add ingredients dynamically.

--- burger-builder/src/components/Burger/Burger.js
+++ burger-builder/src/components/Burger/Burger.js
@@ -3,11 +3,16 @@ import styles from './Burger.module.css'
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
 
 const burger = (props) => {
+  const transformedIngredients = Object.keys(props.ingredients).map(ingredientKey => {
+    return [...Array(props.ingredients[ingredientKey])].map((_, index) => {
+      return <BurgerIngredient key={ingredientKey + index} type={ingredientKey} />
+    })
+  })
+
   return (
     <div className={styles.burger}>
       <BurgerIngredient type="top-bun" />
-      <BurgerIngredient type="cheese" />
-      <BurgerIngredient type="meat" />
+      {transformedIngredients}
       <BurgerIngredient type="bottom-bun" />
     </div>
   )
