f9cbab796f38f8473882b69b5551d0df82da813f
Kayla Sween <kayla@kaylasween.com>
2019-12-16 12:37:41 -0600

    adding custom button component.

--- burger-builder/src/components/Burger/OrderSummary/OrderSummary.js
+++ burger-builder/src/components/Burger/OrderSummary/OrderSummary.js
@@ -16,6 +16,8 @@ const orderSummary = (props) => {
         {ingredientSummary}
       </ul>
       <p>Continue to Checkout?</p>
+      <button onClick={props.show}>CANCEL</button>
+      <button>CONTINUE</button>
     </>
   )
 }
---
+++ burger-builder/src/components/UI/Button/Button.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import styles from './Button.module.css'
+
+const button = (props) => (
+  <button className={[styles.button, styles[props.buttonType]].join(' ')} onClick={props.clicked}>{props.children}</button>
+)
+
+export default button
