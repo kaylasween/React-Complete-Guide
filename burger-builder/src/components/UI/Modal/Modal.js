b713facf53223cb3c57f6fb316abac24a4f63b41
Kayla Sween <kayla@kaylasween.com>
2019-12-16 12:05:44 -0600

    adding a backdrop when modal is shown.

--- burger-builder/src/components/UI/Modal/Modal.js
+++ burger-builder/src/components/UI/Modal/Modal.js
@@ -1,13 +1,17 @@
 import React from 'react'
+import Backdrop from '../Backdrop/Backdrop'
 import styles from './Modal.module.css'
 
 const modal = (props) => (
-  <div role="dialog" className={styles.modal} style={{
-    transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-    opacity: props.show ? '1' : '0'
-  }} >
-    {props.children}
-  </div>
+  <>
+    <Backdrop show={props.show} clicked={props.modalClosed} />
+    <div role="dialog" className={styles.modal} style={{
+      transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
+      opacity: props.show ? '1' : '0'
+    }} >
+      {props.children}
+    </div>
+  </>
 )
 
 export default modal
