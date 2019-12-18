990a9fe2fe035d0234db2e001b3a6b56ad0c3182
Kayla Sween <kayla@kaylasween.com>
2019-12-18 10:57:32 -0600

    adding toolbar to application.

--- burger-builder/src/components/Layout/Layout.js
+++ burger-builder/src/components/Layout/Layout.js
@@ -2,9 +2,12 @@ import React from 'react'
 
 import styles from './Layout.module.css'
 
+import Toolbar from '../Navigation/Toolbar/Toolbar'
+
 const layout = (props) => {
   return (
     <>
+      <Toolbar />
       <div>Toolbar, Sidebar, Backdrop</div>
       <main className={styles.content}>
         {props.children}
---
+++ burger-builder/src/components/Navigation/Toolbar/Toolbar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import styles from './Toolbar.module.css'
+
+const toolbar = (props) => (
+  <header className={styles.toolbar}>
+    <div>MENU</div>
+    <div>LOGO</div>
+    <nav>
+      
+    </nav>
+  </header>
+)
+
+export default toolbar
