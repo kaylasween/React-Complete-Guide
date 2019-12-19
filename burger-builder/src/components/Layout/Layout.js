e46fdc3a30d9c5cd992c4b02876048daba8b888a
Kayla Sween <kayla@kaylasween.com>
2019-12-19 12:33:06 -0600

    adding ability to close side drawer.

--- burger-builder/src/components/Layout/Layout.js
+++ burger-builder/src/components/Layout/Layout.js
@@ -1,20 +1,35 @@
-import React from 'react'
+import React, { Component } from 'react'
 
 import styles from './Layout.module.css'
 
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer'
 import Toolbar from '../Navigation/Toolbar/Toolbar'
 
-const layout = (props) => {
-  return (
-    <>
-      <Toolbar />
-      <SideDrawer />
-      <main className={styles.content}>
-        {props.children}
-      </main>
-    </>
-  )
+class Layout extends Component {
+  state = {
+    showSideDrawer: true
+  }
+
+  sideDrawerOpenHandler = () => {
+    this.setState({ showSideDrawer: true })
+  }
+
+  sideDrawerCloseHandler = () => {
+    this.setState({ showSideDrawer: false })
+  }
+
+  render(){
+    return (
+      <>
+        <Toolbar />
+        <SideDrawer close={this.sideDrawerCloseHandler} open={this.state.showSideDrawer} />
+        <main className={styles.content}>
+          {this.props.children}
+        </main>
+      </>
+    )
+  }
+  
 }
 
-export default layout
+export default Layout
