1cb6c7a3e0cbb0022a837b7c29223adbce14b7a7
Kayla Sween <kayla@kaylasween.com>
2019-12-22 18:59:10 -0600

    removing menu text, adding hamburger icon (with visually hidden text for the icon)

--- burger-builder/src/components/Navigation/SideDrawer/MenuToggle/MenuToggle.js
+++ burger-builder/src/components/Navigation/SideDrawer/MenuToggle/MenuToggle.js
@@ -1,7 +1,14 @@
 import React from 'react'
 
+import styles from './MenuToggle.module.css'
+
 const menuToggle = (props) => (
-  <div onClick={props.toggle}>MENU</div>
+  <div className={styles.drawerToggle} onClick={props.toggle}>
+    <span className="visually-hidden">Open Menu</span>
+    <div></div>
+    <div></div>
+    <div></div>
+  </div>
 )
 
 export default menuToggle
