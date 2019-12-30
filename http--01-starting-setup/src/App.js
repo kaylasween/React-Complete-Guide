b366d37642fce8868ef7721fd107704bf63a564d
Kayla Sween <kayla@kaylasween.com>
2019-12-30 14:59:24 -0600

    learning about routing and server deployment and adding assignment files.

--- http--01-starting-setup/src/App.js
+++ http--01-starting-setup/src/App.js
@@ -6,7 +6,9 @@ import Blog from './containers/Blog/Blog'
 class App extends Component {
   render() {
     return (
-      <BrowserRouter>
+      <BrowserRouter basename="/">
+        {/* basename="/" is the default, and doesn't need to be set, but would need to be set if 
+        serving app from sub directory (basename="my-app") */}
         <div className="App">
           <Blog />
         </div>
