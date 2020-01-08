fdd1137602237272e3c9f54eed2edf5a4d9c3d62
Kayla Sween <kayla@kaylasween.com>
2020-01-08 13:12:53 -0600

    connecting redux to react.

--- redux--01-start/src/index.js
+++ redux--01-start/src/index.js
@@ -1,8 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { createStore } from 'redux'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import './index.css'
+import App from './App'
+import reducer from './store/reducer'
+import registerServiceWorker from './registerServiceWorker'
+
+const store = createStore(reducer)
+
+ReactDOM.render(<App />, document.getElementById('root'))
+registerServiceWorker()
