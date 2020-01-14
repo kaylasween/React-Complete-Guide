8b4cb31bec84c89780012e825250f5bb6d123e82
Kayla Sween <kayla@kaylasween.com>
2020-01-14 12:38:11 -0600

    making action types into constants and storing them in their own file to avoid typos.

--- redux--01-start/src/store/reducer.js
+++ redux--01-start/src/store/reducer.js
@@ -1,41 +1,43 @@
+import * as actionTypes from './actions'
+
 const initialState = {
   counter: 0,
   results: []
 }
 
 const reducer = (state = initialState, action) => {
-  if (action.type === 'INCREMENT') {
+  if (action.type === actionTypes.INCREMENT) {
     // const newState = Object.assign({}, state)
     return {
       ...state,
       counter: state.counter + 1
     }
   }
-  if (action.type === 'DECREMENT') {
+  if (action.type === actionTypes.DECREMENT) {
     return {
       ...state,
       counter: state.counter - 1
     }
   }
-  if (action.type === 'ADD') {
+  if (action.type === actionTypes.ADD) {
     return {
       ...state,
       counter: state.counter + action.value
     }
   }
-  if (action.type === 'SUBTRACT') {
+  if (action.type === actionTypes.SUBTRACT) {
     return {
       ...state,
       counter: state.counter - action.value
     }
   }
-  if (action.type === 'STORE_RESULT') {
+  if (action.type === actionTypes.STORE_RESULT) {
     return {
       ...state,
       results: state.results.concat({ value: state.counter, id: new Date() }) //don't use push() to avoid mutating the original array
     }
   }
-  if (action.type === 'DELETE_RESULT') {
+  if (action.type === actionTypes.DELETE_RESULT) {
     // const id = 2
     // const newArray = [...state.results]
     // newArray.splice(id, 1)
