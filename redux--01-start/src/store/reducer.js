1f030481542318e2775490cf77d2d5556c14d0b8
Kayla Sween <kayla@kaylasween.com>
2020-01-14 12:32:05 -0600

    adding ability to store and remove results. learning how to immutably update arrays.

--- redux--01-start/src/store/reducer.js
+++ redux--01-start/src/store/reducer.js
@@ -1,26 +1,49 @@
 const initialState = {
-  counter: 0
+  counter: 0,
+  results: []
 }
 
 const reducer = (state = initialState, action) => {
   if (action.type === 'INCREMENT') {
+    // const newState = Object.assign({}, state)
     return {
+      ...state,
       counter: state.counter + 1
     }
   }
   if (action.type === 'DECREMENT') {
     return {
+      ...state,
       counter: state.counter - 1
     }
   }
   if (action.type === 'ADD') {
     return {
+      ...state,
       counter: state.counter + action.value
     }
   }
   if (action.type === 'SUBTRACT') {
     return {
+      ...state,
       counter: state.counter - action.value
+    }
+  }
+  if (action.type === 'STORE_RESULT') {
+    return {
+      ...state,
+      results: state.results.concat({ value: state.counter, id: new Date() }) //don't use push() to avoid mutating the original array
+    }
+  }
+  if (action.type === 'DELETE_RESULT') {
+    // const id = 2
+    // const newArray = [...state.results]
+    // newArray.splice(id, 1)
+
+    const newArray = state.results.filter((element) => element.id !== action.resultId)
+    return {
+      ...state,
+      results: newArray
     }
   }
   return state
