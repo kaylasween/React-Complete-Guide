017ca90f8f695fc05af4e7348a2a0c2e15130566
Kayla Sween <kayla@kaylasween.com>
2020-01-17 10:44:16 -0600

    splitting up reducers.

---
+++ redux--01-start/src/store/reducers/result.js
@@ -0,0 +1,28 @@
+import * as actionTypes from '../actions'
+
+const initialState = {
+  results: []
+}
+
+const reducer = (state = initialState, action) => {
+  if (action.type === actionTypes.STORE_RESULT) {
+    return {
+      ...state,
+      results: state.results.concat({ value: action.result, id: new Date() }) //don't use push() to avoid mutating the original array
+    }
+  }
+  if (action.type === actionTypes.DELETE_RESULT) {
+    // const id = 2
+    // const newArray = [...state.results]
+    // newArray.splice(id, 1)
+
+    const newArray = state.results.filter((element) => element.id !== action.resultId)
+    return {
+      ...state,
+      results: newArray
+    }
+  }
+  return state
+}
+
+export default reducer
