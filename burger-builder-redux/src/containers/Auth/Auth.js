10240c75b2ef8a35dd00b58290a4b0920e4bacc9
Kayla Sween <kayla@kaylasween.com>
2020-02-03 14:05:10 -0600

    finishing adding the authentication page. Still WIP.

--- burger-builder-redux/src/containers/Auth/Auth.js
+++ burger-builder-redux/src/containers/Auth/Auth.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import Input from '../../components/UI/Input/Input'
 import Button from '../../components/UI/Button/Button'
 
+import styles from './Auth.module.css'
+
 class Auth extends Component {
   state = {
     controls: {
@@ -36,6 +38,36 @@ class Auth extends Component {
     }
   }
 
+  checkValidatity(value, rules) {
+    let valid = true
+
+    //could also add a check for true instead of adding empty validation to delivery methods
+
+    if (rules.required) {
+      valid = value.trim() !== '' && valid
+    }
+    if (rules.minLength) {
+      valid = value.length >= rules.minLength && valid
+    }
+    if (rules.maxLength) {
+      valid = value.length <= rules.maxLength && valid
+    }
+    return valid
+  }
+
+  inputChangedHandler = (event, controlName) => {
+    const updatedAuthForm = {
+      ...this.state.controls,
+      [controlName]: {
+        ...this.state.controls[controlName],
+        value: event.target.value,
+        valid: this.checkValidatity(event.target.value, this.state.controls[controlName].validation),
+        touched: true
+      }
+    }
+    this.setState({ controls: updatedAuthForm })
+  }
+
   render() {
     const formElements = []
     for (let key in this.state.controls) {
@@ -47,18 +79,19 @@ class Auth extends Component {
 
     const form = formElements.map(formElement => (
       <Input key={formElement.id}
-        elementType={formEl.config.elementType}
-        elementConfig={formEl.config.elementConfig}
-        id={formEl.id}
-        invalid={!formEl.config.valid}
-        shouldValidate={formEl.config.validation}
-        touched={formEl.config.touched}
-        value={formEl.config.value}
-        label={formEl.config.label}
-        changed={(event) => this.inputChangedHandler(event, formEl.id)} />
+        elementType={formElement.config.elementType}
+        elementConfig={formElement.config.elementConfig}
+        id={formElement.id}
+        invalid={!formElement.config.valid}
+        shouldValidate={formElement.config.validation}
+        touched={formElement.config.touched}
+        value={formElement.config.value}
+        label={formElement.config.label}
+        changed={(event) => this.inputChangedHandler(event, formElement.id)} />
     ))
     return (
-      <form>
+      <form className={styles.auth}>
+        <h2>Sign in/Sign up</h2>
         {form}
         <Button buttonType="cta">Submit</Button>
       </form>
