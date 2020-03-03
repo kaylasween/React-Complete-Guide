462d2dcb0b5a26301681ad8fdd409cb705f0c919
Kayla Sween <kayla@kaylasween.com>
2020-03-03 10:15:11 -0600

    removing build from gitignore.

---
+++ burger-builder-redux/build/precache-manifest.5f7d259a2f743338a3805728b10b2c41.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "62175927f399b6b095f6",
+    "url": "/static/css/main.4b0e59c5.chunk.css"
+  },
+  {
+    "revision": "62175927f399b6b095f6",
+    "url": "/static/js/main.62175927.chunk.js"
+  },
+  {
+    "revision": "f0d4e098f6afd10511aa",
+    "url": "/static/css/1.c68e0d20.chunk.css"
+  },
+  {
+    "revision": "f0d4e098f6afd10511aa",
+    "url": "/static/js/1.f0d4e098.chunk.js"
+  },
+  {
+    "revision": "1eb4c7d0144c6f5c089d",
+    "url": "/static/css/2.53943e15.chunk.css"
+  },
+  {
+    "revision": "1eb4c7d0144c6f5c089d",
+    "url": "/static/js/2.1eb4c7d0.chunk.js"
+  },
+  {
+    "revision": "adc65e0a55fcdce5b7a6",
+    "url": "/static/css/3.04930de5.chunk.css"
+  },
+  {
+    "revision": "adc65e0a55fcdce5b7a6",
+    "url": "/static/js/3.adc65e0a.chunk.js"
+  },
+  {
+    "revision": "a91699755469617ede45",
+    "url": "/static/js/4.a9169975.chunk.js"
+  },
+  {
+    "revision": "ad7a3e7e2e28940d4b44",
+    "url": "/static/js/runtime~main.ad7a3e7e.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/static/media/burger-logo.b8503d26.png"
+  },
+  {
+    "revision": "9c88ac0cdee1ba455a63b2f68a9b637e",
+    "url": "/index.html"
+  }
+];
