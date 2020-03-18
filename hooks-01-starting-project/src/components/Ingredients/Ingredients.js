139781e536b5822e3b6f7618cd04eea5ac261855
Kayla Sween <kayla@kaylasween.com>
2020-03-18 14:13:39 -0500

    adding some efficiency to the filter by using a timeout to avoid sending multiple requests and clearing the old timer when a new one is set.

--- hooks-01-starting-project/src/components/Ingredients/Ingredients.js
+++ hooks-01-starting-project/src/components/Ingredients/Ingredients.js
@@ -7,23 +7,8 @@ import Search from './Search'
 const Ingredients = () => {
   const [ingredients, setIngredients] = useState([])
 
-  // runs after and for every render cycle
-  useEffect(() => {
-    fetch('https://react-hooks-58945.firebaseio.com/ingredients.json')
-      .then(response => {
-        return response.json()
-      }).then(responseData => {
-        const loadedIngredients = []
-        for (const key in responseData) {
-          loadedIngredients.push({
-            id: key,
-            title: responseData[key].title,
-            amount: responseData[key].amount
-          })
-        }
-        setIngredients(loadedIngredients)
-      })
-  }, []) // empty array of external dependencies as second argument causes it to act like componentDidMount()
+  // useEffect runs after and for every render cycle
+  // empty array of external dependencies as second argument causes useEffect to act like componentDidMount()
   // otherwise, when something is in the array that changes, it reruns.
 
   const filteredIngredientsHandler = useCallback((filteredIngs) => {
--- hooks-01-starting-project/src/components/Ingredients/Search.js
+++ hooks-01-starting-project/src/components/Ingredients/Search.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 import Card from '../UI/Card';
 import './Search.css';
@@ -6,31 +6,39 @@ import './Search.css';
 const Search = React.memo(props => {
   const { onLoadIngredients } = props
   const [filter, setFilter] = useState('')
+  const filterRef = useRef()
 
   useEffect(() => {
-    const query = filter.length === 0 ? '' : `?orderBy="title"&equalTo="${filter}"`
-    fetch('https://react-hooks-58945.firebaseio.com/ingredients.json' + query)
-      .then(response => {
-        return response.json()
-      }).then(responseData => {
-        const loadedIngredients = []
-        for (const key in responseData) {
-          loadedIngredients.push({
-            id: key,
-            title: responseData[key].title,
-            amount: responseData[key].amount
+    const timer = setTimeout(() => {
+      if (filter === filterRef.current.value) {
+        const query = filter.length === 0 ? '' : `?orderBy="title"&equalTo="${filter}"`
+        fetch('https://react-hooks-58945.firebaseio.com/ingredients.json' + query)
+          .then(response => {
+            return response.json()
+          }).then(responseData => {
+            const loadedIngredients = []
+            for (const key in responseData) {
+              loadedIngredients.push({
+                id: key,
+                title: responseData[key].title,
+                amount: responseData[key].amount
+              })
+            }
+            onLoadIngredients(loadedIngredients)
           })
-        }
-        onLoadIngredients(loadedIngredients)
-      })
-  }, [filter, onLoadIngredients])
+      }
+    }, 500)
+    return () => {
+      clearTimeout(timer)
+    } //runs when component is unmounted if empty useEffect dependency array
+  }, [filter, onLoadIngredients, filterRef])
 
   return (
     <section className="search">
       <Card>
         <div className="search-input">
           <label>Filter by Title</label>
-          <input type="text" value={filter} onChange={(event) => setFilter(event.target.value)} />
+          <input ref={filterRef} type="text" value={filter} onChange={(event) => setFilter(event.target.value)} />
         </div>
       </Card>
     </section>
