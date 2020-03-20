0eecb43bde437e664930edfff59b177c75f5302a
Kayla Sween <kayla@kaylasween.com>
2020-03-20 17:05:43 -0500

    finished hooks lectures after a wild goose of a typo bug.

--- hooks-01-starting-project/src/components/Ingredients/Ingredients.js
+++ hooks-01-starting-project/src/components/Ingredients/Ingredients.js
@@ -1,9 +1,10 @@
-import React, { useReducer, useCallback, useMemo } from 'react'
+import React, { useReducer, useEffect, useCallback, useMemo } from 'react'
 
-import ErrorModal from '../UI/ErrorModal'
 import IngredientForm from './IngredientForm'
 import IngredientList from './IngredientList'
+import ErrorModal from '../UI/ErrorModal'
 import Search from './Search'
+import useFetch from '../../hooks/fetch'
 
 // may be better to use a reducer in instances where you rely on previous state to set the new state.
 const ingredientReducer = (currentIngredients, action) => {
@@ -13,100 +14,87 @@ const ingredientReducer = (currentIngredients, action) => {
     case 'ADD':
       return [...currentIngredients, action.ingredient]
     case 'DELETE':
-      return currentIngredients.filter(ingredient => ingredient.id !== action.id)
+      return currentIngredients.filter(ing => ing.id !== action.id)
     default:
-      throw new Error('needs to set, add, or delete')
-  }
-}
-
-// may also make sense when you multiple connected states.
-const httpReducer = (httpState, action) => {
-  switch (action.type) {
-    case 'SEND':
-      return { loading: true, error: null }
-    case 'RESPOND':
-      return { ...httpState, loading: false }
-    case 'ERROR':
-      return { loading: false, error: action.errorMessage }
-    case 'CLEAR':
-      return { ...httpState, error: null }
-    default:
-      throw new Error('Should be send, respond, or error')
+      throw new Error('Should not get there!')
   }
 }
 
 const Ingredients = () => {
-  const [ingredients, dispatchIngs] = useReducer(ingredientReducer, [])
-  const [httpState, httpDispatch] = useReducer(httpReducer, { loading: false, error: null })
+  const [userIngredients, dispatch] = useReducer(ingredientReducer, [])
+  const {
+    isLoading,
+    error,
+    data,
+    sendRequest,
+    reqExtra,
+    requestIdentifier,
+    clear
+  } = useFetch()
 
-  // const [ingredients, setIngredients] = useState([]) //replacing with useReducer
-  // const [loading, setLoading] = useState(false)
-  // const [error, setError] = useState()
 
   // useEffect runs after and for every render cycle
   // empty array of external dependencies as second argument causes useEffect to act like componentDidMount()
   // otherwise, when something is in the array that changes, it reruns.
 
-  const filteredIngredientsHandler = useCallback((filteredIngs) => {
-    // setIngredients(filteredIngs)
-    dispatchIngs({ type: 'SET', ingredients: filteredIngs })
+  useEffect(() => {
+    if (!isLoading && !error && requestIdentifier === 'REMOVE_INGREDIENT') {
+      dispatch({ type: 'DELETE', id: reqExtra })
+    } else if (!isLoading && !error && requestIdentifier === 'ADD_INGREDIENT') {
+      dispatch({
+        type: 'ADD',
+        ingredient: { id: data.name, ...reqExtra }
+      })
+    }
+  }, [data, reqExtra, requestIdentifier, isLoading, error])
+
+  const filteredIngredientsHandler = useCallback(filteredIngredients => {
+    dispatch({ type: 'SET', ingredients: filteredIngredients })
   }, [])
 
-  const addIngredientHandler = useCallback((ingredient) => {
-    httpDispatch({ type: 'SEND' })
-    // setLoading(true)
-    fetch('https://react-hooks-58945.firebaseio.com/ingredients.json', {
-      method: 'POST',
-      body: JSON.stringify(ingredient),
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    }).then(response => {
-      // setLoading(false)
-      httpDispatch({ type: 'RESPOND' })
-      return response.json()
-    }).then(responseData => {
-      // setIngredients(prevIngs => [...prevIngs, { id: responseData.name, ...ingredient }])
-      dispatchIngs({ type: 'ADD', ingredient: { id: responseData.name, ...ingredient } })
-    })
-  }, [])
+  const addIngredientHandler = useCallback(ingredient => {
+    sendRequest(
+      'https://react-hooks-58945.firebaseio.com/ingredients.json',
+      'POST',
+      JSON.stringify(ingredient),
+      ingredient,
+      'ADD_INGREDIENT'
+    )
+  }, [sendRequest])
 
-  const removeIngredientHandler = useCallback((ingredientId) => {
-    // setLoading(true)
-    httpDispatch({ type: 'SEND' })
-    console.log(ingredientId)
-    fetch(`https://react-hooks-58945.firebaseio.com/ingredients/${ingredientId}.json`, {
-      method: 'DELETE'
-    }).then(response => {
-      // setLoading(false)
-      httpDispatch({ type: 'RESPOND' })
-      // setIngredients(prevIngs => prevIngs.filter(item => ingredientId !== item.id))
-      dispatchIngs({ type: 'DELETE', id: ingredientId })
-    }).catch(error => {
-      // setError("Something went wrong! " + error.message)
-      // setLoading(false)
-      httpDispatch({ type: 'ERROR', errorMessage: error.message })
-    })
-  }, [])
-
-  const clearError = useCallback(() => {
-    // setError(null)
-    httpDispatch({ type: 'CLEAR' })
-  }, [])
+  const removeIngredientHandler = useCallback(
+    ingredientId => {
+      sendRequest(
+        `https://react-hooks-58945.firebaseio.com/ingredients/${ingredientId}.json`,
+        'DELETE',
+        null,
+        ingredientId,
+        'REMOVE_INGREDIENT'
+      )
+    },
+    [sendRequest]
+  )
 
   /* use useMemo when something is getting rerendered that doesn't need to be, especially if that function is 
-     doing something a bit more computationally intensive. If it's a trivial component, it may be worth it to not 
-     use it */
+      doing something a bit more computationally intensive. If it's a trivial component, it may be worth it to not 
+      use it */
   const ingredientList = useMemo(() => {
     return (
-      <IngredientList ingredients={ingredients} onRemoveItem={(ingredientId) => removeIngredientHandler(ingredientId)} />
+      <IngredientList
+        ingredients={userIngredients}
+        onRemoveItem={removeIngredientHandler}
+      />
     )
-  }, [ingredients, removeIngredientHandler])
+  }, [userIngredients, removeIngredientHandler])
 
   return (
     <div className="App">
-      {httpState.error && <ErrorModal onClose={clearError}>{httpState.error}</ErrorModal>}
-      <IngredientForm onAddIngredient={addIngredientHandler} loading={httpState.loading} />
+      {error && <ErrorModal onClose={clear}>{error}</ErrorModal>}
+
+      <IngredientForm
+        onAddIngredient={addIngredientHandler}
+        loading={isLoading}
+      />
 
       <section>
         <Search onLoadIngredients={filteredIngredientsHandler} />
--- hooks-01-starting-project/src/components/Ingredients/Search.js
+++ hooks-01-starting-project/src/components/Ingredients/Search.js
@@ -1,43 +1,49 @@
 import React, { useState, useEffect, useRef } from 'react';
 
 import Card from '../UI/Card';
+import ErrorModal from '../UI/ErrorModal'
+import useFetch from '../../hooks/fetch'
 import './Search.css';
 
 const Search = React.memo(props => {
   const { onLoadIngredients } = props
   const [filter, setFilter] = useState('')
   const filterRef = useRef()
+  const { isLoading, data, error, sendRequest, clear } = useFetch()
 
   useEffect(() => {
     const timer = setTimeout(() => {
       if (filter === filterRef.current.value) {
         const query = filter.length === 0 ? '' : `?orderBy="title"&equalTo="${filter}"`
-        fetch('https://react-hooks-58945.firebaseio.com/ingredients.json' + query)
-          .then(response => {
-            return response.json()
-          }).then(responseData => {
-            const loadedIngredients = []
-            for (const key in responseData) {
-              loadedIngredients.push({
-                id: key,
-                title: responseData[key].title,
-                amount: responseData[key].amount
-              })
-            }
-            onLoadIngredients(loadedIngredients)
-          })
+        sendRequest('https://react-hooks-58945.firebaseio.com/ingredients.json' + query)
       }
     }, 500)
     return () => {
       clearTimeout(timer)
     } //runs when component is unmounted if empty useEffect dependency array
-  }, [filter, onLoadIngredients, filterRef])
+  }, [filter, filterRef, sendRequest])
+
+  useEffect(() => {
+    if (!isLoading && !error && data) {
+      const loadedIngredients = []
+      for (const key in data) {
+        loadedIngredients.push({
+          id: key,
+          title: data[key].title,
+          amount: data[key].amount
+        })
+      }
+      onLoadIngredients(loadedIngredients)
+    }
+  }, [data, isLoading, error, onLoadIngredients])
 
   return (
     <section className="search">
+      {error && <ErrorModal onClose={clear}>{error}</ErrorModal>}
       <Card>
         <div className="search-input">
           <label>Filter by Title</label>
+          {isLoading && <span>Loading...</span>}
           <input ref={filterRef} type="text" value={filter} onChange={(event) => setFilter(event.target.value)} />
         </div>
       </Card>
