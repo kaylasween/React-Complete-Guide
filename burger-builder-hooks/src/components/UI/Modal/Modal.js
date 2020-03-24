2449a0a0d69f2facf9a7c2e05bdb38f7d14d9221
Kayla Sween <kayla@kaylasween.com>
2020-03-24 14:46:24 -0500

    wrapping modal component with React.memo to replace componentShouldUpdate.

--- burger-builder-hooks/src/components/UI/Modal/Modal.js
+++ burger-builder-hooks/src/components/UI/Modal/Modal.js
@@ -2,33 +2,21 @@ import React, { useEffect } from 'react'
 import Backdrop from '../Backdrop/Backdrop'
 import styles from './Modal.module.css'
 
-const modal = (props) => {
-  // shouldComponentUpdate(nextProps, nextState) {
-  //   return (
-  //     nextProps.show !== this.props.show ||
-  //     nextProps.children !== this.props.children
-  //   )
-  // }
+const modal = (props) => (
+  <>
+    <Backdrop show={props.show} clicked={props.modalClosed} />
+    <div
+      role="dialog"
+      className={styles.modal}
+      style={{
+        transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
+        opacity: props.show ? '1' : '0'
+      }}
+    >
+      {props.children}
+    </div>
+  </>
+)
 
-  useEffect(() => {
-
-  }, [])
-
-  return (
-    <>
-      <Backdrop show={props.show} clicked={props.modalClosed} />
-      <div
-        role="dialog"
-        className={styles.modal}
-        style={{
-          transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-          opacity: props.show ? '1' : '0'
-        }}
-      >
-        {props.children}
-      </div>
-    </>
-  )
-}
-
-export default modal
+//React.memo, in this instance, replaces componentShouldUpdate
+export default React.memo(modal, (prevProps, nextProps) => nextProps.show === prevProps.show && nextProps.children === prevProps.children)
